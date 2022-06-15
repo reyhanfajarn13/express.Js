2a067798981ac4bc11ba84c343a7c404ce38f67f
reyhanfajarn13 <reyhanfajarn13@gmail.com>
2022-06-15 18:29:45 +0700

    rebuild gitignore

--- app.js
+++ app.js
@@ -13,8 +13,8 @@ app.use(routerAdmin);
 
 app.use(routerShop);
 app.use((req,res,next) => {
-    res.status(404).send("<h1>404 <br>PAGE NOT FOUND</br></h1>");
-})
+    res.status(404).send("<h1>404 <br>PAGE NOT FOUND</br></h1>")
+});
 
 
 
